676ebf4ff72e06092d3ef2b1f7ae143aaa890286
支豪亮 <zhihaoliang@t-ark.com>
2020-02-11 14:24:52 +0800

    项目初始化

---
+++ baseframe/src/main/java/com/t_ark/baseframe/ui/BaseActivity.kt
@@ -0,0 +1,12 @@
+package com.t_ark.baseframe.ui
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * 创建日期：2020-02-11
+ * 描述:所有Activity的基类，用于处理一些公共方法
+ * 作者:支豪亮
+ */
+class BaseActivity :AppCompatActivity(){
+
+}
---
+++ baseframe/src/main/java/com/t_ark/baseframe/ui/BaseFragment.kt
@@ -0,0 +1,13 @@
+package com.t_ark.baseframe.ui
+
+import androidx.fragment.app.Fragment
+
+/**
+ * 创建日期：2020-02-11
+ * 描述:所有Fragment的基类处理Fragment中一些公共方法
+ * 作者:支豪亮
+ */
+
+class BaseFragment :Fragment(){
+
+}
